a44c5974727dd8462ee05df0259466a6b2423ff2
LunLi <xidianli@qq.com>
2019-01-29 16:58:56 +0800

    time count down

--- NatureSound/NatureSound/UI/TimeViewController.swift
+++ NatureSound/NatureSound/UI/TimeViewController.swift
@@ -9,9 +9,9 @@
 import UIKit
 import LGButton
 import NSLogger
+import SnapKit
 
-
-class TimeViewController: UIViewController {
+class TimeViewController: UIViewController, DPTimePickerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,18 +25,36 @@ class TimeViewController: UIViewController {
         
         
         Logger.shared.log(.model, .info, "modifyTimeCounddown vc")
-                
+        
+        let container = UIView.init(frame: CGRect.init(x: view.safeAreaInsets.left,
+                                                       y: view.safeAreaInsets.top,
+                                                       width: view.bounds.width - view.safeAreaInsets.left - view.safeAreaInsets.right,
+                                                       height: view.bounds.height - view.safeAreaInsets.top - view.safeAreaInsets.bottom))
+        container.backgroundColor = UIColor.clear
+        container.tag = 1234
+        view.addSubview(container)
+        container.snp.makeConstraints { (make) in
+            make.edges.equalTo(view.safeAreaLayoutGuide.snp.edges)
+        }
 
         let timePicker: DPTimePicker = DPTimePicker.timePicker()
-        timePicker.insertInView(view)
+        timePicker.insertInView(container)
         
         /*
          Customization goes here
          */
-        
+        timePicker.delegate = self
         timePicker.show(nil)
         
     }
 
-
+    func timePickerDidConfirm(_ hour: String, minute: String, timePicker: DPTimePicker) {
+        Logger.shared.log(.model, .info, "Confirm vc hour \(hour) minute \(minute)")
+        view.viewWithTag(1234)!.removeFromSuperview()
+    }
+    
+    func timePickerDidClose(_ timePicker: DPTimePicker) {
+        Logger.shared.log(.model, .info, "Cancel vc")
+        view.viewWithTag(1234)!.removeFromSuperview()
+    }
 }
